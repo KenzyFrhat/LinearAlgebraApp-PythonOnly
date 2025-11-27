a644c38a94406bd78392bf30605c787aeebec224
KenzyFrhat <kenzyfrhat723@gmail.com>
2025-11-27 08:38:09 +0200

    Explain the algorithm of the transpose function

--- app/core/transpose.py
+++ app/core/transpose.py
@@ -1,4 +1,6 @@
 def transpose_matrix(A):
-    m = len(A)
-    n = len(A[0]) if m>0 else 0
-    return [[A[i][j] for i in range(m)] for j in range(n)]
+    rows = len(A) # number of rows 
+    columns = len(A[0]) if rows >0 else 0  # number of coumns , the if condition is to handle empty matrix case cause len(A[0]) will give index error if A is empty
+    return [[A[i][j] for i in range(rows)] for j in range(columns)]  # we went through the original matrix column by column and created new rows for the transposed matrix
+     
+
